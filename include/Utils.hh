9dbc3f776f95379ab928b5b11a15630f06ff9476
Arseniy Simonov <ars@exasol.com>
2018-08-13 13:02:33 +0200

    dprobe: removed boost_program_options dependency

--- include/Utils.hh
+++ include/Utils.hh
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <limits.h>
+
 namespace dprobe {
 
 class NonCopyable {
@@ -21,5 +23,34 @@ protected:
     Singleton() = default;
     ~Singleton() = default;
 };
+
+static inline uint64_t str_to_u64(std::string s) {
+    if (s.empty())
+        throw std::runtime_error("empty argument value");
+
+    uint64_t multi = 1;
+    auto last = *s.rbegin();
+    if (!isdigit(last)) {
+        switch (last) {
+        case 'T':   multi *= 1024;
+                    // fall through
+        case 'G':   multi *= 1024;
+                    // fall through
+        case 'M':   multi *= 1024;
+                    // fall through
+        case 'K':   multi *= 1024; break;
+        default: throw std::runtime_error("invalid argument value");
+        }
+        s.pop_back();
+    }
+
+    char* e;
+    auto res = strtoull(s.c_str(), &e, 10);
+
+    if (*e or res == ULLONG_MAX)
+        throw std::runtime_error("invalid argument value");
+
+    return res*multi;
+}
 
 }
--- src/main.cc
+++ src/main.cc
@@ -1,67 +1,67 @@
 #include <include/Dispatcher.hh>
+#include <include/Utils.hh>
 
 #include <stdio.h>
+#include <argp.h>
 #include <iostream>
 #include <boost/property_tree/ini_parser.hpp>
-#include <boost/program_options.hpp>
 
+const char *argp_program_version = "dprobe.0.1";
+const char *argp_program_bug_address = "harvos.arsen@gmail.com";
+static char doc[] = "dprobe - a resource accessibility checker";
+static const char* args_doc = "";
+
+struct ProgOptions {
+    std::string config{};
+    std::string out_file{};
+    uint64_t log_level{2};
+    uint64_t max_sampling_rate_ms{100};
+};
+
+static struct argp_option optdesc[] = {
+    {"config",'c',                  "PATH", 0, "path to config file", 0},
+    {"out-file",'o',                "PATH", 0, "path to log file", 0},
+    {"log-level",'l',               "NUMBER", 0, "0-4, 0: only fatal, 4: everything", 0},
+    {"max-sampling-rate-ms",'s',    "NUMBER", 0, "how often does main thread takes looks at the clock, msec", 0},
+    {0, 0, 0, 0, 0, 0}
+};
+
+auto parse_opt (int key, char *arg, struct argp_state *state) -> error_t
+{
+    auto opts = reinterpret_cast<ProgOptions*>(state->input);
+
+    switch (key) {
+    case 'c':   opts->config = arg; break;
+    case 'o':   opts->out_file = arg; break;
+    case 'l':   opts->log_level = dprobe::str_to_u64(arg); break;
+    case 's':   opts->max_sampling_rate_ms = dprobe::str_to_u64(arg); break;
+    default: return ARGP_ERR_UNKNOWN;
+    }
+
+    return 0;
+}
 
 int main(int argc, char** argv) try {
     using namespace dprobe;
-    namespace po = boost::program_options;
 
-    std::string config_file;
-    po::options_description desc("Allowed options");
-    uint32_t log_level;
-    uint32_t max_sampling_rate_ms;
-    const std::string stdout_keyword("<stdout>");
-    std::string log_file;
+    ProgOptions opts;
+    struct argp argp = { optdesc, parse_opt, args_doc, doc, NULL, NULL, NULL };
+    argp_parse (&argp, argc, argv, 0, 0, reinterpret_cast<void*>(&opts));
 
-    desc.add_options()
-
-        ("help",
-            "produce help message")
-
-        ("config,c",
-            po::value(&config_file),
-            "path to config file")
-
-        ("out-file,o",
-            po::value(&log_file)->default_value(stdout_keyword),
-            "path to log file")
-
-        ("log-level,l",
-            po::value(&log_level)->default_value(2),
-            "0-4, 0: only fatal, 4: everything")
-
-        ("max-sampling-rate,s", 
-            po::value(&max_sampling_rate_ms)->default_value(100),
-            "how often does main thread takes looks at the clock, msec");
-
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-        std::cout << desc << "\n";
-        exit(EXIT_SUCCESS);
-    }
-
-    if (log_file != stdout_keyword) {
-        if (freopen(log_file.c_str(), "w+", stdout) == NULL)
+    if (!opts.out_file.empty()) {
+        if (freopen(opts.out_file.c_str(), "w+", stdout) == NULL)
             throw std::runtime_error("Failed to open logfile");
     }
 
-    if (config_file.empty())
+    if (opts.config.empty())
         throw std::invalid_argument("config file not specified");
 
-    log::Logger::get().filter((log::Level) log_level);
+    log::Logger::get().filter((log::Level) opts.log_level);
 
     probe::Options root;
-    boost::property_tree::ini_parser::read_ini(config_file, root);
+    boost::property_tree::ini_parser::read_ini(opts.config, root);
 
-    dprobe::Dispatcher cfg(max_sampling_rate_ms, root);
+    dprobe::Dispatcher cfg(opts.max_sampling_rate_ms, root);
     cfg.loop();
     return 0;
 } catch(const std::exception& x) {
